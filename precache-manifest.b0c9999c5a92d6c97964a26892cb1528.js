e7a38facd1db6f50e1428fd66f96b2d7c6eeb01e
blue_baney <bluebaney10@gmail.com>
2019-01-07 19:25:16 +0700

    Updates

---
+++ precache-manifest.b0c9999c5a92d6c97964a26892cb1528.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac9dd74a1cb8fa83e295",
+    "url": "/react-web-tutorial/static/js/runtime~main.ac9dd74a.js"
+  },
+  {
+    "revision": "42871eb6ba6ff69b7f4f",
+    "url": "/react-web-tutorial/static/js/main.42871eb6.chunk.js"
+  },
+  {
+    "revision": "cb17f997c4faaf6e2217",
+    "url": "/react-web-tutorial/static/js/1.cb17f997.chunk.js"
+  },
+  {
+    "revision": "cb17f997c4faaf6e2217",
+    "url": "/react-web-tutorial/static/css/1.46a5ca9d.chunk.css"
+  },
+  {
+    "revision": "2f38bd91112f0a6483144adcfc2edc58",
+    "url": "/react-web-tutorial/index.html"
+  }
+];
